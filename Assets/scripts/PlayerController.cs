bc28b6cdfb2b160b35333f67387443d217253a93
Jules Gribble <gribblej@me.com>
2018-01-30 16:21:05 +0000

    Inital Commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float panSpeed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(moveHorizontal * Time.deltaTime * speed, 0f, moveVertical * Time.deltaTime*speed);
+
+        Camera mycam = GetComponent<Camera>();
+        float sensitivity = 0.05f;
+        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
+        vp.x -= 0.5f;
+        vp.y -= 0.5f;
+        vp.x *= sensitivity;
+        vp.y *= sensitivity;
+        vp.x += 0.5f;
+        vp.y += 0.5f;
+        Vector3 sp = mycam.ViewportToScreenPoint(vp);
+
+        Vector3 v = mycam.ScreenToWorldPoint(sp);
+        transform.LookAt(v, Vector3.up);
+    }
+}
