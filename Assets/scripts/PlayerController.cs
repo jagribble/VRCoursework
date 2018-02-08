fdefe5f7751c94cef2c5d2ad380e88ee3822a34c
Jules Gribble <gribblej@me.com>
2018-02-08 17:45:38 +0000

    final

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -25,33 +25,19 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-		float mouseX = Input.GetAxis("Mouse X");
-		float mouseY = Input.GetAxis("Mouse Y");
-
-
 
         // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         transform.Translate(moveHorizontal * Time.deltaTime * speed, 0f, moveVertical * Time.deltaTime*speed);
 
 
 		//https://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera
-		yaw += speedH * Input.GetAxis("Mouse X");
+	
+	
 		pitch -= speedV * Input.GetAxis("Mouse Y");
+		yaw += speedH * Input.GetAxis("Mouse X");
+
 
 		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-//
-//        Camera mycam = GetComponent<Camera>();
-//        float sensitivity = 0.05f;
-//        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
-//        vp.x -= 0.5f;
-//        vp.y -= 0.5f;
-//        vp.x *= sensitivity;
-//        vp.y *= sensitivity;
-//        vp.x += 0.5f;
-//        vp.y += 0.5f;
-//        Vector3 sp = mycam.ViewportToScreenPoint(vp);
-//
-//        Vector3 v = mycam.ScreenToWorldPoint(sp);
-//        transform.LookAt(v, Vector3.up);
+
     }
 }
