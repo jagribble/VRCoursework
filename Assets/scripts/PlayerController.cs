333b9ab29692e81246e5569daaf32dd814aaef2c
Jules Gribble <gribblej@me.com>
2018-02-08 13:10:52 +0000

    added another way of camera movement

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     public float panSpeed;
     private Rigidbody rb;
 
+
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,21 +24,34 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+
+
         // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         transform.Translate(moveHorizontal * Time.deltaTime * speed, 0f, moveVertical * Time.deltaTime*speed);
 
-        Camera mycam = GetComponent<Camera>();
-        float sensitivity = 0.05f;
-        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
-        vp.x -= 0.5f;
-        vp.y -= 0.5f;
-        vp.x *= sensitivity;
-        vp.y *= sensitivity;
-        vp.x += 0.5f;
-        vp.y += 0.5f;
-        Vector3 sp = mycam.ViewportToScreenPoint(vp);
 
-        Vector3 v = mycam.ScreenToWorldPoint(sp);
-        transform.LookAt(v, Vector3.up);
+		//https://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera
+		yaw += speedH * Input.GetAxis("Mouse X");
+		pitch -= speedV * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+//
+//        Camera mycam = GetComponent<Camera>();
+//        float sensitivity = 0.05f;
+//        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
+//        vp.x -= 0.5f;
+//        vp.y -= 0.5f;
+//        vp.x *= sensitivity;
+//        vp.y *= sensitivity;
+//        vp.x += 0.5f;
+//        vp.y += 0.5f;
+//        Vector3 sp = mycam.ViewportToScreenPoint(vp);
+//
+//        Vector3 v = mycam.ScreenToWorldPoint(sp);
+//        transform.LookAt(v, Vector3.up);
     }
 }
